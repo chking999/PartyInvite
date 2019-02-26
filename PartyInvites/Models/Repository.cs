55f84f9752ec17bfc80030c1f594c135bfebf91b
Christian King <christian.king@accenture.com>
2019-02-26 14:50:56 -0600

    End Chapter 2

---
+++ PartyInvites/Models/Repository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PartyInvites.Models
+{
+    public static class Repository
+    {
+        public static readonly List<GuestResponse> Responses = new List<GuestResponse>();
+
+        public static void AddResponse(GuestResponse response)
+        {
+            Responses.Add(response);
+        }
+    }
+}
